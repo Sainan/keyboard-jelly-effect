19f81393dbe0c729fd983a2d3aecdaec41c66a4d
Sainan <sainan@calamity.gg>
2023-10-25 13:32:56 +0200

    Support Razer analogue keyboards

--- main.cpp
+++ main.cpp
@@ -3,14 +3,16 @@
 #include <AnalogueKeyboard.hpp>
 #include <console.hpp>
 #include <kbRgb.hpp>
+#include <Keyboard.hpp>
 #include <Thread.hpp>
 #include <Vector2.hpp>
 
 using namespace soup;
 
 static bool running = true;
+static AnalogueKeyboard* ka; // Using a pointer because compiler is too stupid to move it
 static UniquePtr<kbRgb> kr;
-static Rgb colours[6 * 21];
+static Rgb colours[kbRgb::MAX_ROWS * kbRgb::MAX_COLUMNS];
 
 int main()
 {
@@ -31,54 +33,63 @@ int main()
 		}
 		exit(0);
 	});
-	for (auto& _kr : kbRgb::getAll())
+	for (auto& kbd : Keyboard::getAll())
 	{
-		kr = std::move(_kr);
-		kr->init();
+		if (!kbd.analogue || !kbd.rgb)
+		{
+			continue;
+		}
+		std::cout << "Input: " << kbd.analogue->name << "\n";
+		std::cout << "Output: " << kbd.rgb->name << "\n";
+		ka = &*kbd.analogue;
+		kr = std::move(kbd.rgb);
 		Thread t([]
 		{
-			for (auto& ka : AnalogueKeyboard::getAll())
+			while (running)
 			{
-				while (running)
+				// Map intensity
+				uint8_t intensity[kbRgb::MAX_ROWS * kbRgb::MAX_COLUMNS]{ 0 };
+				for (auto& key : ka->getActiveKeys())
 				{
-					// Map intensity
-					uint8_t intensity[6 * 21]{ 0 };
-					for (auto& key : ka.getActiveKeys())
+					auto [row, column] = kr->mapKeyToPos(key.sk);
+					if (row != 0xff && column != 0xff)
 					{
-						auto [row, column] = kr->mapKeyToPos(key.sk);
-						if (row != 0xff && column != 0xff)
+						Vector2 impact_point(row, column);
+						for (uint8_t row = 0; row != kr->getNumRows(); ++row)
 						{
-							Vector2 impact_point(row, column);
-							for (uint8_t row = 0; row != kr->getNumRows(); ++row)
+							for (uint8_t column = 0; column != kbRgb::MAX_COLUMNS; ++column)
 							{
-								for (uint8_t column = 0; column != kr->getNumColumns(); ++column)
+								const float dist = impact_point.distance(Vector2(row, column));
+								if (dist <= 10.0f)
 								{
-									const float dist = impact_point.distance(Vector2(row, column));
-									if (dist <= 10.0f)
-									{
-										const Percentage<uint8_t> closeness = (1.0f - (dist / 10.0f));
+									const Percentage<uint8_t> closeness = (1.0f - (dist / 10.0f));
 #if true // additive
-										auto key_intensity = static_cast<unsigned int>(key.value * closeness);
-										key_intensity += intensity[row * kr->getNumColumns() + column];
-										intensity[row * kr->getNumColumns() + column] = static_cast<uint8_t>(std::min(255u, key_intensity));
+									auto key_intensity = static_cast<unsigned int>(key.value * closeness);
+									key_intensity += intensity[row * kbRgb::MAX_COLUMNS + column];
+									intensity[row * kbRgb::MAX_COLUMNS + column] = static_cast<uint8_t>(std::min(255u, key_intensity));
 #else
-										auto key_intensity = static_cast<uint8_t>(key.value * closeness);
-										if (key_intensity > intensity[row * kr->getNumColumns() + column])
-										{
-											intensity[row * kr->getNumColumns() + column] = key_intensity;
-										}
-#endif
+									auto key_intensity = static_cast<uint8_t>(key.value * closeness);
+									if (key_intensity > intensity[row * kbRgb::MAX_COLUMNS + column])
+									{
+										intensity[row * kbRgb::MAX_COLUMNS + column] = key_intensity;
 									}
+#endif
 								}
 							}
 						}
 					}
+				}
 
-					// Map intensity to colours
-					for (size_t i = 0; i != 6 * 21; ++i)
-					{
-						colours[i] = Rgb(0, intensity[i], 0);
-					}
+				// Map intensity to colours
+				for (size_t i = 0; i != kbRgb::MAX_ROWS * kbRgb::MAX_COLUMNS; ++i)
+				{
+					colours[i] = Rgb(0, intensity[i], 0);
+				}
+
+				if (ka->disconnected)
+				{
+					std::cout << "Keyboard disconnected\n";
+					running = false;
 				}
 			}
 		});
@@ -86,10 +97,17 @@ int main()
 		{
 			// Map colours to keys
 			Rgb keys[NUM_KEYS];
-			kr->mapPosToKeys(keys, colours, 6, 21);
+			kr->mapPosToKeys(keys, colours, kbRgb::MAX_ROWS, kbRgb::MAX_COLUMNS);
 			kr->setKeys(keys);
 
 			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
+		break;
 	}
+	if (running)
+	{
+		std::cout << "No compatible (analogue + RGB) keyboard found.\n";
+		return 1;
+	}
+	return 0;
 }
